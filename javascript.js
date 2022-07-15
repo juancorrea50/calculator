9f66f77e23819ae7746730a22dd778e2a38cb3b8
Juan Correa <juancorrea50@yahoo.com>
2022-07-15 06:26:01 -0500

    Change operator button logic and start logic for operate function
    Was still running into issues with more than one operator appearing and
    opted to just disable the buttons for the operators with a single click
    of any operator. Also adjusted numberChoice array to concatenate it's
    containers to reflect the display. Operate function will have the same
    logic before applying arithmetic to the values.

--- javascript.js
+++ javascript.js
@@ -1,72 +1,68 @@
-const operatorArray = ['+','-','*','/'];
-const numberChoice = [0];
+let operatorChoice = '';
+const numberChoice = [];
 
-const buttons = document.querySelector('button');
+
 const displayInput = document.getElementById('result');
-
+const buttonSwitch = document.getElementsByClassName('op-button');
+const calculateButton = document.querySelector('.calculate')
 
 
 function display(value){
-    
-        //Repeated values do not show up (1 bug: Only covers one  extra click of the same operator)
-        if(value == '+' && numberChoice[(numberChoice.length-1)] == '+'|| value == '-' && numberChoice[(numberChoice.length-1)] == '-' 
-        || value == '*' && numberChoice[(numberChoice.length-1)] == '*' || value == '/' && numberChoice[(numberChoice.length-1)] == '/'
-        || value == '.' && numberChoice[(numberChoice.length-1)] == '.'){
-            //Doesn't add the selection to the displayInput variable and pops the choice from the numberChoice array
-            for(x=0;x<numberChoice.length;x++){
-                if(numberChoice[x]== '+' || numberChoice[x]== '-' || numberChoice[x]== '*' || numberChoice[x]== '/' || numberChoice[x]== '.'){
-                    console.log(displayInput.value);
-                }
-            }
-        } else{
-            //Adds the input to the value to be displayed on the element in the HTML
-            displayInput.value += value;
 
+        //Disabled the buttons after hitting one operator
+        if(value == '+' || value == '-' || value == '*' || value == '/'){
+            for(x=0;x<buttonSwitch.length;x++){
+                buttonSwitch[x].disabled = true;
+            }
+            operatorChoice = value;
+            //Makes the first value in numberChoice Array the first number in the pair of numbers and removes excess data to save memory
+            for(x=1;x<numberChoice.length;x++){
+                numberChoice[0] += numberChoice[x];
+            }
+            numberChoice.splice(1, numberChoice.length);
+            console.log(numberChoice)
+            console.log(operatorChoice);
+        } else {
             numberChoice.push(value);
             console.log(numberChoice);
+
         }
+
+        //Adds the input to the value to be displayed on the element in the HTML
+        displayInput.value += value;
+
+
  
 
 }
 
 
+
 function clearDisplay(){
+    //Clears display and numberChoice Array
+    numberChoice.splice(0,numberChoice.length);
+    console.log(numberChoice);
     displayInput.value = "";
+
+    //Re enables operator buttons
+    for(x=0;x<buttonSwitch.length;x++){
+        buttonSwitch[x].disabled = false;
+    }
+
 }
 
 
 
 
 
-//Arithmetic functions
-const add = function(num1, num2){
-    return num1 + num2;
-};
+function operate() {
+    for(x=2;x<numberChoice.length;x++){
+        numberChoice[1] += numberChoice[x];
+    }
+    numberChoice.splice(2, numberChoice.length);
 
-console.log("Add "+add(2,4));
-
-const subtract = function(num1, num2){
-    return num1 - num2;
-};
-
-console.log("Subtract "+subtract(2,4));
-
-const multiply = function(num1, num2){
-    return num1 * num2;
-};
-
-console.log("Multiply "+ multiply(2,4));
-
-const divide = function(num1,num2){
-    if(num2 == 0){
-        return "DO NOT DIVIDE BY ZERO"
-    };
-    return num1/num2;
-};
-
-console.log("Divide " + divide(2,4));
-
-const operate = function(op, num1, num2) {
+    console.log(numberChoice);
+/*
     if(op == '+'){
         return add(num1, num2);
     } else if(op == '-'){
@@ -78,6 +74,34 @@ const operate = function(op, num1, num2) {
     } else {
         return "Skipped";
     }
+    */
 };
 
-console.log(operate("/", 2, 2));
+
+
+
+
+calculateButton.addEventListener('click', operate());
+
+
+
+
+//Arithmetic functions
+const add = function(num1, num2){
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+};
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+};
+
+const divide = function(num1,num2){
+    if(num2 == 0){
+        return "DO NOT DIVIDE BY ZERO"
+    };
+    return num1/num2;
+};
