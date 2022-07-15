397045a333bd9bf24577c0a537f6ef686beffc4a
Juan Correa <juancorrea50@yahoo.com>
2022-07-14 23:50:01 -0500

    Modify javascript to include operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let num1;
+let num2;
+//Arithmetic functions
 const add = function(num1, num2){
     return num1 + num2;
 };
@@ -12,20 +15,31 @@ console.log("Subtract "+subtract(2,4));
 
 const multiply = function(num1, num2){
     return num1 * num2;
-}
+};
 
 console.log("Multiply "+ multiply(2,4));
 
 const divide = function(num1,num2){
     if(num2 == 0){
-        return "NOPE"
+        return "DO NOT DIVIDE BY ZERO"
     };
     return num1/num2;
 };
 
 console.log("Divide " + divide(2,4));
-/*
-const operate = function((), num1, num2) {
-    if(() = )
-}
-*/
+
+const operate = function(op, num1, num2) {
+    if(op == '+'){
+        return add(num1, num2);
+    } else if(op == '-'){
+        return subtract(num1,num2);
+    } else if(op == '*'){
+        return multiply(num1,num2);
+    } else if(op == '/'){
+        return divide(num1,num2);
+    } else {
+        return "Skipped";
+    }
+};
+
+console.log(operate("/", 2, 2));
