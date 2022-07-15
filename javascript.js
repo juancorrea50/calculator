2be84ff9eee9d5f724eb1e9d1f7a39ff3ce930a6
Juan Correa <juancorrea50@yahoo.com>
2022-07-15 06:45:15 -0500

    Change operate function click to inline HTML and adjust math functions
    Was experiencing difficulty with the addEventListener for the button
    created with the querySelector. For some reason the operate function was
    instantly called without input. Will research on reason later. For now
    all buttons are called with the inline html onclick tag. Math functions
    are functional for Int value numbers and dividing by zero will show the
    error message.

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const numberChoice = [];
 
 const displayInput = document.getElementById('result');
 const buttonSwitch = document.getElementsByClassName('op-button');
-const calculateButton = document.querySelector('.calculate')
+
 
 
 function display(value){
@@ -52,7 +52,25 @@ function clearDisplay(){
 }
 
 
+//Arithmetic functions
+const add = function(num1, num2){
+    return parseInt(num1) + parseInt(num2);
+};
 
+const subtract = function(num1, num2){
+    return parseInt(num1) - parseInt(num2);
+};
+
+const multiply = function(num1, num2){
+    return parseInt(num1) * parseInt(num2);
+};
+
+const divide = function(num1,num2){
+    if(parseInt(num2) == 0){
+        return "DO NOT DIVIDE BY ZERO"
+    };
+    return num1/num2;
+};
 
 
 function operate() {
@@ -60,48 +78,20 @@ function operate() {
         numberChoice[1] += numberChoice[x];
     }
     numberChoice.splice(2, numberChoice.length);
-
+    console.log('operate activated')
     console.log(numberChoice);
-/*
-    if(op == '+'){
-        return add(num1, num2);
-    } else if(op == '-'){
-        return subtract(num1,num2);
-    } else if(op == '*'){
-        return multiply(num1,num2);
-    } else if(op == '/'){
-        return divide(num1,num2);
-    } else {
-        return "Skipped";
+
+    if(operatorChoice == '+'){
+        displayInput.value = add(numberChoice[0], numberChoice[1]);
+    } else if(operatorChoice == '-'){
+        displayInput.value = subtract(numberChoice[0],numberChoice[1]);
+    } else if(operatorChoice == '*'){
+        displayInput.value = multiply(numberChoice[0],numberChoice[1]);
+    } else if(operatorChoice == '/'){
+        displayInput.value =  divide(numberChoice[0],numberChoice[1]);
     }
-    */
 };
 
 
 
 
-
-calculateButton.addEventListener('click', operate());
-
-
-
-
-//Arithmetic functions
-const add = function(num1, num2){
-    return num1 + num2;
-};
-
-const subtract = function(num1, num2){
-    return num1 - num2;
-};
-
-const multiply = function(num1, num2){
-    return num1 * num2;
-};
-
-const divide = function(num1,num2){
-    if(num2 == 0){
-        return "DO NOT DIVIDE BY ZERO"
-    };
-    return num1/num2;
-};
